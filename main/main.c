e27c4e194af4a0b0784e86eeb380c420c22bcdd3
Greppijonathan <jonathangreppi@gmail.com>
2025-03-31 21:17:15 -0300

    TEC1 Implementada

--- main/main.c
+++ main/main.c
@@ -1,3 +1,13 @@
+/*
+Requisitos mínimos: Implementar un cronómetro utilizando más de una tarea de FreeRTOS. El
+cronómetro debe iniciar y detener la cuenta al presionar la tecla TEC1. Si está detenido, al
+presionar la tecla TEC2 debe volver a cero. Mientras la cuenta está activa el red RGB debe
+parpadear en verde y cuando está detenida debe permanecer en rojo. Mientras la cuenta está
+activa se debe congelar el valor parcial con la tecla TEC3, es decir que se mantiene el valor que
+se muestra en pantalla fijo pero la cuenta continúa activa. En este estado, al presionar
+nuevamente la tecla TEC3 se recupera el valor actual de la cuenta en vivo.
+*/
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/semphr.h"
@@ -6,19 +16,23 @@
 #include "stdio.h"
 #include "driver/gpio.h"
 
-// Definiciones para el manejo de los digitos de la pantalla tft
+// Definiciones para el manejo de los dígitos de la pantalla TFT
 #define DIGITO_ANCHO 60
 #define DIGITO_ALTO 100
 #define DIGITO_ENCENDIDO ILI9341_BLUE2
 #define DIGITO_APAGADO 0x3800
 #define DIGITO_FONDO ILI9341_BLACK
 
-// Pines del led RGB
+// Pines del LED RGB
 #define RGB_ROJO GPIO_NUM_1
 #define RGB_VERDE GPIO_NUM_8
 #define RGB_AZUL GPIO_NUM_10
 
-// Estructura para el manejo de los digitos del cronometro, en este caso es el recurso compartido que se debe cuidar al usar semaforos
+// Pines conectados a pulsadores
+#define TEC1 GPIO_NUM_6
+#define TEC2 GPIO_NUM_4
+
+// Estructura para el manejo de los dígitos del cronómetro
 struct digitos_cronometro
 {
     int unidades_segundos;
@@ -27,51 +41,54 @@ struct digitos_cronometro
     int decenas_minutos;
 } *digitosActuales_t;
 
-// Se decalara el semaforo
+volatile int estadoVerde = 0;
+// Declaración del semáforo
 SemaphoreHandle_t semaforo;
+
+// Variable global para pausar o reanudar cronómetro
+volatile bool pausaCronometro = false;
+// Variable global para pausar o reanaudar cuenta
+volatile bool reiniciarCuenta = false;
 
 void ControlTiempo(void *parametros)
 {
+    bool estadoLed = false; // Control del parpadeo del LED
     while (1)
     {
-
-        if (xSemaphoreTake(semaforo, portMAX_DELAY)) // Si el semaforo indica que el recurso compartido esta disponible, se accede a modificar los digitos
+        if (!pausaCronometro) // Si el cronómetro no está en pausa
         {
-            digitosActuales_t
-                ->unidades_segundos = (digitosActuales_t
-                                           ->unidades_segundos +
-                                       1) %
-                                      10;
-            if (digitosActuales_t
-                    ->unidades_segundos == 0)
+            if (xSemaphoreTake(semaforo, portMAX_DELAY)) // Acceso al recurso compartido
             {
-                digitosActuales_t
-                    ->decenas_segundos = (digitosActuales_t
-                                              ->decenas_segundos +
-                                          1) %
-                                         6;
-                if (digitosActuales_t
-                        ->decenas_segundos == 0)
+                // Actualizar los dígitos del cronómetro
+                digitosActuales_t->unidades_segundos = (digitosActuales_t->unidades_segundos + 1) % 10;
+                if (digitosActuales_t->unidades_segundos == 0)
                 {
-                    digitosActuales_t
-                        ->unidades_minutos = (digitosActuales_t
-                                                  ->unidades_minutos +
-                                              1) %
-                                             10;
-                    if (digitosActuales_t
-                            ->unidades_minutos == 0)
+                    digitosActuales_t->decenas_segundos = (digitosActuales_t->decenas_segundos + 1) % 6;
+                    if (digitosActuales_t->decenas_segundos == 0)
                     {
-                        digitosActuales_t
-                            ->decenas_minutos = (digitosActuales_t
-                                                     ->decenas_minutos +
-                                                 1) %
-                                                6;
+                        digitosActuales_t->unidades_minutos = (digitosActuales_t->unidades_minutos + 1) % 10;
+                        if (digitosActuales_t->unidades_minutos == 0)
+                        {
+                            digitosActuales_t->decenas_minutos = (digitosActuales_t->decenas_minutos + 1) % 6;
+                        }
                     }
                 }
+                xSemaphoreGive(semaforo); // Liberar el recurso
             }
-            xSemaphoreGive(semaforo); // Se libera el recurso, se lo indica en el semaforo
+
+            gpio_set_level(RGB_VERDE, estadoLed);
+            estadoLed = !estadoLed;
+            gpio_set_level(RGB_AZUL, 0);
+            gpio_set_level(RGB_ROJO, 0);
         }
-        // Cada un segundo ejecutamos el incremento de las variables del cronometro
+        // Parpadeo LED azul mientras está en funcionamiento
+        if (pausaCronometro)
+        {
+            gpio_set_level(RGB_AZUL, estadoLed);
+            estadoLed = !estadoLed;
+            gpio_set_level(RGB_VERDE, 0);
+            gpio_set_level(RGB_ROJO, 0);
+        }
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
@@ -82,7 +99,7 @@ void ActualizarPantalla(void *parametros)
     ILI9341Init();
     ILI9341Rotate(ILI9341_Landscape_1);
 
-    // Se crean los paneles-dígitos para mostrar los segmentos prendidos
+    // Se crean los paneles-dígitos
     panel_t horas = CrearPanel(30, 60, 2, DIGITO_ALTO, DIGITO_ANCHO, DIGITO_ENCENDIDO, DIGITO_APAGADO, DIGITO_FONDO);
     panel_t minutos = CrearPanel(170, 60, 2, DIGITO_ALTO, DIGITO_ANCHO, DIGITO_ENCENDIDO, DIGITO_APAGADO, DIGITO_FONDO);
 
@@ -95,10 +112,9 @@ void ActualizarPantalla(void *parametros)
         int unidadesAnteriorSegundos;
     } digitosPrevios = {-1, -1, -1, -1}; // Valores iniciales "invalidados"
 
-    // En esta seccion se muestran en pantalla los digitos y se verifica si hubo un cambio para poder optimizar la actualizacion
     while (1)
     {
-        if (xSemaphoreTake(semaforo, portMAX_DELAY)) // Se accede al recurso compartido cuando está disponible
+        if (xSemaphoreTake(semaforo, portMAX_DELAY)) // Acceso al recurso compartido, la estructura de los digitos en este caso
         {
             if (digitosActuales_t->decenas_minutos != digitosPrevios.decenasAnteriorMinutos)
             {
@@ -128,36 +144,68 @@ void ActualizarPantalla(void *parametros)
     }
 }
 
+// Tarea para controlar el pulsador
+void EscanearPulsadores(void *parametros)
+{
+    bool estadoAnteriorPulsadorTEC1 = true; // Estado anterior del pulsador TEC1
+    bool estadoAnteriorPulsadorTEC2 = true; // Estado anterior del pulsador TEC2
+    while (1)
+    {
+        // Escaneo del pulsador TEC1
+        bool estadoActualTEC1 = gpio_get_level(TEC1);        // Leer el estado actual de TEC1
+        if (!estadoActualTEC1 && estadoAnteriorPulsadorTEC1) // Detecta flanco de bajada
+        {
+            pausaCronometro = !pausaCronometro; // Alternar pausa del cronómetro
+        }
+        estadoAnteriorPulsadorTEC1 = estadoActualTEC1; // Actualizar el estado anterior para TEC1
+
+        // Escaneo del pulsador TEC2
+        /*       bool estadoActualTEC2 = gpio_get_level(TEC2);        // Leer el estado actual de TEC2
+               if (!estadoActualTEC2 && estadoAnteriorPulsadorTEC2) // Detecta flanco de bajada
+               {
+                   reiniciarCuenta = !reiniciarCuenta; // Alternar reinicio de cuenta
+               }
+               estadoAnteriorPulsadorTEC2 = estadoActualTEC2; // Actualizar el estado anterior para TEC2
+
+               // Retraso para debounce*/
+        vTaskDelay(pdMS_TO_TICKS(50));
+    }
+}
+
 void app_main(void)
 {
-    // Se inicializan los digitos del cronometro
+    // Se inicializan los dígitos del cronómetro
     digitosActuales_t = malloc(sizeof(struct digitos_cronometro));
-    digitosActuales_t
-        ->unidades_segundos = 0;
-    digitosActuales_t
-        ->decenas_segundos = 0;
-    digitosActuales_t
-        ->unidades_minutos = 0;
-    digitosActuales_t
-        ->decenas_minutos = 0;
+    digitosActuales_t->unidades_segundos = 0;
+    digitosActuales_t->decenas_segundos = 0;
+    digitosActuales_t->unidades_minutos = 0;
+    digitosActuales_t->decenas_minutos = 0;
 
-    // Se declaran como salida los pines del led RGB
+    // Configuración de los pines del LED RGB
     gpio_set_direction(RGB_ROJO, GPIO_MODE_OUTPUT);
     gpio_set_direction(RGB_VERDE, GPIO_MODE_OUTPUT);
     gpio_set_direction(RGB_AZUL, GPIO_MODE_OUTPUT);
-
-        gpio_set_level(RGB_ROJO, 0);
+    gpio_set_level(RGB_ROJO, 0);
     gpio_set_level(RGB_VERDE, 0);
-    gpio_set_level(RGB_AZUL, 1);
+    gpio_set_level(RGB_AZUL, 0);
 
-    // Creamos el semaforo
+    // Configuración de los pines de los pulsadores
+    gpio_set_direction(TEC1, GPIO_MODE_INPUT);
+    gpio_set_pull_mode(TEC1, GPIO_PULLUP_ONLY); // Resistencia de pull-up
+
+    gpio_set_direction(TEC2, GPIO_MODE_INPUT);
+    gpio_set_pull_mode(TEC2, GPIO_PULLUP_ONLY); // Resistencia de pull-up
+
+    // Crear el semáforo
     semaforo = xSemaphoreCreateMutex();
 
+    // Crear las tareas
     xTaskCreate(ControlTiempo, "ControlTiempo", 2048, NULL, tskIDLE_PRIORITY + 1, NULL);
     xTaskCreate(ActualizarPantalla, "ActualizarPantalla", 4096, NULL, tskIDLE_PRIORITY + 2, NULL);
+    xTaskCreate(EscanearPulsadores, "LecturaPulsadores", 1024, NULL, tskIDLE_PRIORITY + 1, NULL);
 
     while (1)
     {
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        vTaskDelay(pdMS_TO_TICKS(1000)); // Evitar saturar la CPU
     }
 }
